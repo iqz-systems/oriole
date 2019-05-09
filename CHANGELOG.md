019695372421e658de624dad35358e6dda4e36d1
Rajshri Mohan K S <rajshri.m@iqzsystems.com>
2019-05-09 11:37:16 +0530

    Reverted 'Made all properties of `ProductAttribute` optional.'

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+#### 0.3.1
+
+-   Reverted 'Made all properties of `ProductAttribute` optional.'
+
 ### 0.3.0
 
 -   Made all properties of `ProductAttribute` optional.
--- src/products/product-attribute.ts
+++ src/products/product-attribute.ts
@@ -3,24 +3,24 @@ import { ProductCustomAttribute } from './product-custom-attribute';
 
 export class ProductAttribute {
 
-  id?: number;
-  sku?: string;
-  name?: string;
-  attribute_set_id?: number;
-  price?: number;
-  status?: number;
-  visibility?: number;
-  type_id?: string;
+  id: number = 0;
+  sku: string = '';
+  name: string = '';
+  attribute_set_id: number = 0;
+  price: number = 0;
+  status: number = 0;
+  visibility: number = 0;
+  type_id: string = '';
 
   @Type(() => Date)
-  created_at?: Date;
+  created_at: Date = new Date();
 
   @Type(() => Date)
-  updated_at?: Date;
+  updated_at: Date = new Date();
 
-  product_links?: any[];
-  tier_prices?: any[];
+  product_links: any[] = [];
+  tier_prices: any[] = [];
 
   @Type(() => ProductCustomAttribute)
-  custom_attributes?: ProductCustomAttribute[];
+  custom_attributes: ProductCustomAttribute[] = [];
 }
