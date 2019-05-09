c9323184ae2bef46c1af2bf796e7b279218ef101
Rajshri Mohan K S <rajshri.m@iqzsystems.com>
2019-05-09 13:09:10 +0530

    Added type information for  property in .

--- src/categories/models/category.ts
+++ src/categories/models/category.ts
@@ -1,3 +1,4 @@
+import { Expose, Type } from 'class-transformer';
 import { CustomAttribute } from '../../common-models';
 
 export class Category {
@@ -13,5 +14,23 @@ export class Category {
   path: string = '';
   available_sort_by: any[] = [];
   include_in_menu: boolean = false;
+
+  @Type(() => CustomAttribute)
   custom_attributes: CustomAttribute[] = [];
+
+  /**
+   * Gets the category description.
+   * @method description
+   * @return The category description as HTML text.
+   */
+  @Expose() get description(): string {
+    if (!this.custom_attributes || (this.custom_attributes.length < 1)) {
+      return '';
+    }
+    const descriptionAttr = this.custom_attributes.find(attr => attr.attribute_code == 'description');
+    if (!descriptionAttr) {
+      return '';
+    }
+    return descriptionAttr.value as string;
+  }
 }
