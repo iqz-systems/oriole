e6ee3adec037217c9a748b44cd4d883372a5cf4c
Rajshri Mohan K S <rajshri.m@iqzsystems.com>
2019-05-19 12:58:25 +0530

    Improved typing for `ListResult` model.

--- src/common-models/list-result.ts
+++ src/common-models/list-result.ts
@@ -1,7 +1,13 @@
+import { Type } from 'class-transformer';
+import { SearchCriteria } from './search-criteria';
+
 export class ListResult<T> implements IListResult<T>{
   readonly items: T[] = [];
-  readonly search_criteria: { filter_groups: any[]; } = { filter_groups: [] };
-  readonly total_count: number = 0;
+
+  @Type(() => SearchCriteria)
+  readonly search_criteria: SearchCriteria;
+
+  readonly total_count: number;
 
   constructor(listResult: IListResult<T>, items?: T[]) {
     this.search_criteria = listResult.search_criteria;
@@ -18,6 +24,6 @@ export class ListResult<T> implements IListResult<T>{
 
 interface IListResult<T> {
   items?: T[];
-  search_criteria: { filter_groups: any[]; };
+  search_criteria: SearchCriteria;
   total_count: number;
 }
