82d833ea3a68ec1aff81b720f0cc23a9e3f5791e
Rajshri Mohan K S <rajshri.m@iqzsystems.com>
2019-05-23 12:14:39 +0530

    Added methods to search orders

--- src/orders/orders-client.ts
+++ src/orders/orders-client.ts
@@ -1,9 +1,9 @@
+import { plainToClass } from 'class-transformer';
 import * as util from 'util';
 import { RestClient } from '../rest-client';
 import { ClientBase } from '../client-base';
 import { Orders } from './models';
-import { plainToClass } from 'class-transformer';
-import { ListResult } from '../common-models';
+import { ListResult, SearchCriteria, SearchFilterGroup } from '../common-models';
 
 export class OrdersClient extends ClientBase {
 
@@ -27,6 +27,43 @@ export class OrdersClient extends ClientBase {
     } catch (error) {
       throw error;
     }
+  }
+
+  /**
+   * List all orders matching the provided search criteria from the store.
+   * @method search
+   * @param  searchCriteria A SearchCriteria object denoting the paramters to search for.
+   * @param  pageSize       The number of results to be fetched in one "page" of API response. Default is 9999.
+   * @param  currentPage    The current page of the "pages" of result. Use in conjunction with pageSize.
+   * @return                A list of orders.
+   */
+  async search(searchCriteria: SearchCriteria, pageSize: number = 9999, currentPage: number = 1): Promise<ListResult<Orders>> {
+    const query = `searchCriteria[pageSize]=${pageSize}&searchCriteria[currentPage]=${currentPage}`
+      + `&${searchCriteria.toString()}`;
+    const endpointUrl = util.format('/orders?%s', query);
+    try {
+      const result = await this.restClient.get(endpointUrl);
+      return new ListResult<Orders>(result, plainToClass(Orders, result.items as Orders[]));
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   * List all orders from the store for a given customer.
+   * This method simply applies a search criteria for a customer ID.
+   * @method listByCustomer
+   * @param  customerId     The ID of the customer to search for.
+   * @param  pageSize       The number of results to be fetched in one "page" of API response. Default is 9999.
+   * @param  currentPage    The current page of the "pages" of result. Use in conjunction with pageSize.
+   * @return                A list of orders.
+   */
+  async listByCustomer(customerId: number, pageSize: number = 9999, currentPage: number = 1): Promise<ListResult<Orders>> {
+    const searchCriteria = new SearchCriteria();
+    const filterGroup = new SearchFilterGroup();
+    filterGroup.addSearchFilter('customer_id', customerId, 'eq');
+    searchCriteria.addSearchFilterGroup(filterGroup);
+    return this.search(searchCriteria, pageSize, currentPage);
   }
 
   /**
